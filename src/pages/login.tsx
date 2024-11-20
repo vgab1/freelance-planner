a9af271f9372c2ad5b8f593cd42cf2af057fc2ce
vgab1 <vgabriel.sousa123@gmail.com>
2024-11-20 14:09:44 -0300

    adicionando font e imagem

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,8 @@ import { signInWithGoogle } from "../firebase";
 import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import logo from "../assets/fp-logo.webp";
+import imageHome from "../assets/image-1.jpg";
 
 export default function Login() {
   const [user, setUser] = useState<any>(null);
@@ -20,18 +22,28 @@ export default function Login() {
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
+      <img
+        src={imageHome}
+        alt="Imagem de contabilidade"
+        className="w-96 h-96"
+      />
       <div className="p-8 bg-white rounded-lg shadow-lg w-96">
-        <h1 className="text-2xl font-bold text-center text-blue-600 mb-4">
+        <img
+          src={logo}
+          alt="Freelance Planner Logo"
+          className="w-24 h-24 mb-4"
+        />
+        <h1 className="font-title text-2xl font-bold text-center text-blue-600 mb-4">
           Bem-vindo ao Freelance Planner
         </h1>
-        <p className="text-center text-gray-600 mb-6">
+        <p className="font-text text-center text-gray-600 mb-6">
           Faça login para acessar sua conta
         </p>
         <button
           onClick={handleLogin}
-          className="flex items-center justify-center w-full py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
+          className="font-button flex items-center justify-center w-full py-2 px-4 bg-white text-blue-600 rounded-lg outline"
         >
-          <FcGoogle className="w-5 h-5 mr-2" />
+          <FcGoogle className=" w-5 h-5 mr-2" />
           Login com Google
         </button>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ["Poppins", "sans-serif"],
+        text: ["Roboto", "sans-serif"],
+        button: ["Nunito", "sans-serif"]
+      },
+    },
   },
   plugins: [],
-}
+};
